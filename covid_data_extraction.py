e3b7f43a298c89343a2db5d8302a81d58d5dfc44
hideyuki <automationhidey@gmail.com>
2020-07-12 01:53:41 +0800

    third commit

--- covid_data_extraction.py
+++ covid_data_extraction.py
@@ -2,20 +2,18 @@ import urllib
 import urllib.request
 import json
 import pandas as pd
-
+from elasticsearch import Elasticsearch as es
 
 iso_code_dict = {
     "OWID_KOS": "KOS"
 }
 
 covid_data_link = r"https://covid.ourworldindata.org/data/owid-covid-data.csv"
-# with urllib.request.urlopen(covid_data_link) as ourl:
-    # covid_data = json.loads(ourl.read().decode())
-    # covid_data = pandas.read_csv(ourl.read().decode())
-
 covid_data = pd.read_csv(covid_data_link)
 
 covid_data = covid_data[covid_data['continent'].notna()]
 covid_data = covid_data.replace({"iso_code": iso_code_dict})
-
+covid_data.rename(columns={"location": "country"}, inplace=True)
+covid_data["date"] = pd.to_datetime(covid_data["date"], format="%Y-%m-%d")
 print(covid_data.dtypes)
+# documents = covid_data.to_dict(orient='records')
